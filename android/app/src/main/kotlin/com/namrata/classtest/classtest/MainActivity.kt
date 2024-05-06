5e8f3bd529f5c97d671960b867f1bc4c2e5790fd
Namratasharma2002 <namrata.shar2002@gmail.com>
2024-05-06 12:14:57 +0545

    class test

---
+++ android/app/src/main/kotlin/com/namrata/classtest/classtest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.namrata.classtest.classtest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/grid_view_screen.dart
@@ -0,0 +1,31 @@
+
+
+import 'package:flutter/material.dart';
+
+import '../customwidget/mybutton.dart';
+
+class GridViewScreen extends StatelessWidget {
+  final String name = "Pramesh";
+
+  const GridViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Grid View'),
+      ),
+      body: GridView.count(
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: List.generate(name.length, (index) {
+          return MyButton(
+            text: name[index],
+          );
+        }),
+      ),
+    );
+  }
+}
